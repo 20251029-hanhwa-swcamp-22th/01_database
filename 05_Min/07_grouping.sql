3b12b9eeec13de510e1a0f8a8854a10fcf6079e6
Fragancia <qkrwlls8987@gmail.com>
2025-11-04 17:32:03 +0900

    지민

--- 05_Min/07_grouping.sql
+++ 05_Min/07_grouping.sql
@@ -133,3 +133,21 @@ GROUP BY
     menu_price, category_code
 WITH ROLLUP
 -- ORDER BY category_code ASC;
+
+SELECT
+    a.category_code AS '코드',
+    b.category_name '카테고리명',
+    AVG(a.menu_price) AS '평균금액'
+FROM
+    tbl_menu a
+JOIN
+    tbl_category b
+ON
+    a.category_code = b.category_code
+GROUP BY
+    a.category_code, b.category_name
+HAVING
+    AVG(a.menu_price) >= 8000
+ORDER BY
+    평균금액 ASC
+LIMIT 0,3;
